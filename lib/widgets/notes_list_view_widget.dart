c27ac15d9f68a2a645c168f8c0984119391f6ed1
wisam4tech <135574937+wisam4tech@users.noreply.github.com>
2024-08-22 17:16:04 +0300

    create notes list view

---
+++ lib/widgets/notes_list_view_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        itemCount: 8,
+        itemBuilder: (context, index) {
+          return NoteItem();
+        },
+      ),
+    );
+  }
+}
--- lib/widgets/notes_view_body_widget.dart
+++ lib/widgets/notes_view_body_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar_widget.dart';
 import 'package:notes_app/widgets/note_item.dart';
+import 'package:notes_app/widgets/notes_list_view_widget.dart';
 
 class NotesViewBodyWidget extends StatelessWidget {
   const NotesViewBodyWidget({super.key});
@@ -8,14 +9,14 @@ class NotesViewBodyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(24.0),
+      padding: EdgeInsets.all(24.0),
       child: Column(
         children: [
           SizedBox(
             height: 30,
           ),
           CustomAppBarWidget(),
-          NoteItem(),
+          NotesListView(),
         ],
       ),
     );
