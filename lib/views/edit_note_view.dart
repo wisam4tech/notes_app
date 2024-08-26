f25cc50422bb5b0ef14bcd28265461859d53e392
wisam4tech <135574937+wisam4tech@users.noreply.github.com>
2024-08-26 22:59:46 +0300

    add add note state

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/edit_note_view_body_widget.dart';
-import 'package:notes_app/widgets/notes_view_body_widget.dart';
 
 class EditNoteView extends StatelessWidget {
   const EditNoteView({super.key});
